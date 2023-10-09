2a2f16e59410f391205da29c63563a32110f14c1
ThugsInSuits <1036023528@qq.com>
2023-10-09 16:49:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+    data types
+    undefined, null, boolean ,string,symbol,number, and object
+*/
+
+var myName = "Beau"
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+console.log(pi)
